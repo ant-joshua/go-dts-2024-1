567afbf06624149913dabc62bcfcd4180adb3901
Antonius Joshua <antoniusjoshua47@gmail.com>
2024-03-13 20:36:08 +0700

    feat: add dockerfile

--- sesi_6/cmd/gin/main.go
+++ sesi_6/cmd/gin/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"sesi_6/pkg/database"
 	"sesi_6/pkg/routers"
 
@@ -10,16 +9,16 @@ import (
 
 func main() {
 
-	viper.SetConfigName(".env") // name of config file (without extension)
-	viper.SetConfigType("env")  // REQUIRED if the config file does not have the extension in the name
-	viper.AddConfigPath(".")    // path to look for the config file in
+	// viper.SetConfigName(".env") // name of config file (without extension)
+	viper.SetConfigType("env") // REQUIRED if the config file does not have the extension in the name
+	// viper.AddConfigPath(".")    // path to look for the config file in
 
 	viper.AutomaticEnv()
 
-	err := viper.ReadInConfig() // Find and read the config file
-	if err != nil {             // Handle errors reading the config file
-		panic(fmt.Errorf("fatal error config file: %w", err))
-	}
+	// err := viper.ReadInConfig() // Find and read the config file
+	// if err != nil {             // Handle errors reading the config file
+	// 	panic(fmt.Errorf("fatal error config file: %w", err))
+	// }
 
 	port := ":" + viper.GetString("APP_PORT")
 
